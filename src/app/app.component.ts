5fbba17c48539a6c5090e0e2247110ccd40b4603
vladmir-ACE <dev.vladmir@gmail.com>
2025-05-04 20:17:27 +0000

    update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NavBarComponent } from './shared/components/navBar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterLink, RouterOutlet,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  isCollapsed = false;
+
+
+  
+
+
 }
--- src/app/shared/components/navBar/nav-bar.component.ts
+++ src/app/shared/components/navBar/nav-bar.component.ts
@@ -1,17 +1,115 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
 
 @Component({
-  selector: 'app-nav-bar',
-  imports: [],
+  selector: 'vlad-nav-bar',
+ imports: [NzDrawerModule,NzButtonModule,RouterLink, NzIconModule, NzLayoutModule, NzMenuModule,NzAvatarModule],
   standalone:true,
   template: `
-    <p>
-      nav-bar works!
-    </p>
+   <div class="p-4 text-lg font-bold w-full flex justify-between items-center md:hidden">
+   <div class="flex gap-2 items-center">
+    <div>
+    <nz-avatar nzIcon="user" nzSrc="assets/dalv.jpg" [nzSize]="50"></nz-avatar>
+    </div>
+    <div>
+    VLAD
+    </div>
+   </div>
+   <div>
+      <nz-icon nzType="bars" nzTheme="outline" class="cursor-pointer" (click)="open()"  />
+   </div>
+</div>
+<!--drawer content -->
+<nz-drawer
+      class="text-lg font-bold"
+      [nzClosable]="false"
+      [nzVisible]="visible"
+      nzPlacement="right"
+      nzTitle="Menu"
+      (nzOnClose)="close()"
+    >
+      <ng-container *nzDrawerContent>
+      @for (item of menu; track $index) {
+      <div class="cursor-pointer" [routerLink]="item.route">{{item.title}}</div>
+    }
+      </ng-container>
+</nz-drawer>
+<!--normal div -->
+<div class="w-full h-[104px] text-lg font-bold flex flex-wrap items-center justify-around hidden md:flex">
+<div class="flex items-center gap-2">
+    <div>
+    <nz-avatar nzIcon="user" nzSrc="assets/dalv.jpg" [nzSize]="50"></nz-avatar>
+    </div>
+    VLAD
+   </div>
+  <div class="flex  flex-wrap gap-5  p-4 items-center justify-between">
+    @for (item of menu; track $index) {
+      <div class="cursor-pointer" [routerLink]="item.route">{{item.title}}</div>
+    }
+     
+  </div>
+  <p class="text-white">Test couleur</p>
+  <div>
+     <div class="bg-black flex items-center p-4 cursor-pointer">
+      <div class="text-white">Resume</div>
+     
+     </div>
+  </div>
+
+</div>
   `,
-  styles: ``,
+  styles: `
+  nz-icon{
+  font-size: 30px;
+
+}
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavBarComponent {
+
+
+  menu:MenuItem[]=[
+    {
+      title:"About",
+      route:"about"
+    },
+    {
+      title:"Skills",
+      route:"skills"
+    },
+    {
+      title:"Projects",
+      route:"projects"
+    },
+    {
+      title:"Contact me",
+      route:"contact"
+    }
+
+  ]
+
+  //drawer 
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
 
 }
