446bb5a1b0a82004bfab17eb61ab26eeb05a1677
vladmir-ACE <dev.vladmir@gmail.com>
2025-10-13 21:11:53 +0000

    update

--- src/app/shared/components/head-description/header-description/header-description.component.ts
+++ src/app/shared/components/head-description/header-description/header-description.component.ts
@@ -4,11 +4,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'vlad-header-description',
   imports: [],
   template: `
-   <div class="w-full gap-x-4 px-10 bg-red-200 flex justify-between text-vlad">
-   <div class="flex  flex-col gap-5">
-    <div class="text-[30px]">Hello I’am <span class="font-bold">Lanwi Vladmir.</span></div>
-    <div class="text-[30px]">Full-Stack <span class="font-bold">Web and Mobile Developer</span></div>
-
+   <div class="w-full gap-x-4 px-10  mt-5 flex justify-start ">
+   <div class="flex  justify-center flex-col gap-2 lg:gap-5">
+    <div class="text-medium md:text-large">Hello I’am <span class="font-bold">Lanwi Vladmir.</span></div>
+    <div class="text-medium md:text-large">Full-Stack <span class="font-bold">Web and Mobile Developer</span></div>
+    <div class="text-medium md:text-large">Based in <span class="font-bold">TOGO</span></div>
    </div>
 
    <div class="">
