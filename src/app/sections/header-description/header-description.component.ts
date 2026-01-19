766d68a56e69a9150d17b1abae1f8088761a8439
vladmir-ACE <dev.vladmir@gmail.com>
2026-01-19 22:26:33 +0000

    update

--- src/app/sections/header-description/header-description.component.ts
+++ src/app/sections/header-description/header-description.component.ts
@@ -5,14 +5,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   template: `
    <div class="w-full gap-x-4 px-10  flex justify-between ">
-   <div class="flex  justify-center flex-col gap-2 lg:gap-4">
-    <div class="text-medium md:text-large">Hello I’am <span class="font-bold">Lanwi Vladmir.</span></div>
-    <div class="text-medium md:text-large">Full-Stack <span class="font-bold">Web and Mobile Developer</span></div>
-    <div class="text-medium md:text-large">Based in <span class="font-bold">TOGO</span></div>
-   </div>
 
-   <div class="bg-midnight">
-   <img src="assets/head-frame.png" alt="header-img" width="500" height="596"/>
+   <div class="flex justify-center flex-col gap-2 lg:gap-4">
+      <div class="text-medium md:text-large">
+        👋 I'm <span class="font-bold">Lanwi Vladmir</span>
+      </div>
+      <div class="text-medium md:text-large">
+        Full-Stack <span class="font-bold">Web & Mobile Developer</span>
+      </div>
+      <div class="text-medium md:text-large">
+        Turning ideas into elegant digital experiences — based in <span class="font-bold">Togo</span>
+      </div>
+  </div>
+
+
+   <div>
+     <img src="assets/head-frame.png" alt="header-img" width="500" height="596"/>
    </div>
 
    </div>
